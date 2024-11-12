37e4feffa79072200bafbc51b8519d672a9bea5a
vlfxhd69@naver.com <vlfxhd69@nvaer.com>
2024-11-12 17:13:33 +0900

    feat: 여행성향 그래프 수치 업데이트 기능

--- src/main/java/com/ssafy/trip/travelgraph/mapper/TravelGraphMapper.java
+++ src/main/java/com/ssafy/trip/travelgraph/mapper/TravelGraphMapper.java
@@ -3,9 +3,15 @@ package com.ssafy.trip.travelgraph.mapper;
 import com.ssafy.trip.travelgraph.entity.TravelGraph;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 @Mapper
 public interface TravelGraphMapper {
     TravelGraph findTravelGraphByUserId(Long id);
+
+    void calculateTravelDegree(Map<String, Object> map);
 
     void generateTravelGraph(Long id);
 }
