6df00579d4d4907a2db7d1decd88ede2d0bfc421
vlfxhd69@naver.com <vlfxhd69@nvaer.com>
2024-11-23 01:45:57 +0900

    fix: DB cash_to_dotori 테이블 payment_id 추가

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -140,19 +140,19 @@ CREATE TABLE `purchase`
     FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) on delete cascade
 );
 
-CREATE TABLE `cash_to_dotori`
-(
-    `id`              bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `user_id`         bigint             NOT NULL,
-    `recharge_at`     Date               NOT NULL,
-    `quantity`        bigint             NOT NULL,
-    `order_id`        varchar(255)       NOT NULL,
-    `order_name`      varchar(255)       NOT NULL,
-    `status`          varchar(30)        NOT NULL,
-    `provider`        varchar(30)        NOT NULL,
-    `amount`          bigint             NOT NULL,
-    `discount_amount` bigint             NOT NULL,
-    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) on delete cascade
+CREATE TABLE `cash_to_dotori`(
+                                 `id` bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                                 `user_id`    bigint    NOT NULL,
+                                 `recharge_at`    Date    NOT NULL,
+                                 `quantity`    bigint    NOT NULL,
+                                 `order_id`    varchar(255)    NOT NULL,
+                                 `order_name`    varchar(255)    NOT NULL,
+                                 `status`    varchar(30)    NOT NULL,
+                                 `provider`    varchar(30)    NOT NULL,
+                                 `amount`    bigint    NOT NULL,
+                                 `discount_amount`    bigint    NOT NULL,
+                                 `payment_id`    varchar(255)    NOT NULL,
+								FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)  on delete cascade
 );
 
 
