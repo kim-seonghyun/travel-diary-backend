9e99dce504de57bede6ca60d3e0f4ee570c01780
vlfxhd69@naver.com <vlfxhd69@nvaer.com>
2024-11-25 16:00:52 +0900

    feat: Purchase 코드 리팩토링 및 다이어리 구매 후 도토리 트랜젝션 구현

--- src/main/java/com/ssafy/trip/purchase/mapper/PurchaseMapper.java
+++ src/main/java/com/ssafy/trip/purchase/mapper/PurchaseMapper.java
@@ -1,5 +1,6 @@
 package com.ssafy.trip.purchase.mapper;
 
+import com.ssafy.trip.purchase.dto.request.PurchaseRequest;
 import com.ssafy.trip.purchase.entity.Purchase;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,8 +9,9 @@ import java.util.List;
 
 @Mapper
 public interface PurchaseMapper {
-    void addPurchaseHistory(Purchase purchase);
+    void addPurchaseHistory(PurchaseRequest purchaseRequest);
 
     Purchase isVerifyPurchase(@Param("traveldiaryId") Long traveldiaryId, @Param("userId") Long userId);
+
     List<Purchase> getList(Long userId);
 }
--- src/main/java/com/ssafy/trip/purchase/service/PurchaseService.java
+++ src/main/java/com/ssafy/trip/purchase/service/PurchaseService.java
@@ -1,8 +1,11 @@
 package com.ssafy.trip.purchase.service;
 
+import com.ssafy.trip.purchase.dto.request.PurchaseRequest;
 import com.ssafy.trip.purchase.dto.response.PurchaseResponse;
 import com.ssafy.trip.purchase.entity.Purchase;
 import com.ssafy.trip.purchase.mapper.PurchaseMapper;
+import com.ssafy.trip.user.dto.response.UserResponse;
+import com.ssafy.trip.user.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,14 +13,36 @@ import java.util.List;
 @Service
 public class PurchaseService {
     private final PurchaseMapper purchaseMapper;
+    private final UserService userService;
 
-    public PurchaseService(PurchaseMapper purchaseMapper) {
+    public PurchaseService(PurchaseMapper purchaseMapper, UserService userService) {
         this.purchaseMapper = purchaseMapper;
+        this.userService = userService;
     }
 
-    public void addHistory(Purchase purchase) {
-        purchaseMapper.addPurchaseHistory(purchase);
+    public void addHistory(PurchaseRequest purchaseRequest) {
+        purchaseMapper.addPurchaseHistory(purchaseRequest);
     }
+
+    public void confirmTransaction(PurchaseRequest purchaseRequest) throws IllegalAccessException {
+
+        UserResponse customer = userService.findByUserId(purchaseRequest.getUserId());
+        UserResponse owner = userService.findByUserId(purchaseRequest.getOwnerId());
+
+
+        if (customer == null || owner == null) {
+            throw new IllegalAccessException("잘못된 유저 아이디입니다");
+        }
+
+        Long userDotori = customer.getDotori();
+        if (userDotori < purchaseRequest.getBuyPrice()) {
+            throw new IllegalAccessException("결제 할 도토리가 부족합니다");
+        }
+        // 도토리 거래
+        userService.updateDotoriByUserId(customer.getId(), customer.getDotori() - purchaseRequest.getBuyPrice());
+        userService.updateDotoriByUserId(owner.getId(), owner.getDotori() + purchaseRequest.getBuyPrice());
+    }
+
 
     public boolean isVerifyPurchase(Long traveldiaryId, Long userId){
         Purchase purchaseEntity = purchaseMapper.isVerifyPurchase(traveldiaryId, userId);
